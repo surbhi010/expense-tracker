81e60f1a1cc862090dcaef839badaa17ac85ea2d
Surbhi Agrawal <surbhi.agrawal010@gmail.com>
2019-01-18 13:07:54 +0530

    change in authguard

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -2,31 +2,15 @@ import { Injectable } from '@angular/core';
 import {
   CanActivate,
   ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  Router
+  RouterStateSnapshot, Router
 } from '@angular/router';
+// import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService) { }
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot):
-    Observable<boolean> | Promise<boolean> | boolean {
-    this.authService.isAuthenticated()
-      .then(
-        (authenticated: boolean) => {
-          if (authenticated) {
-            return true;
-          } else {
-            this.router.navigate(['/']);
-          }
-        }
-      );
-    return true;
-  }
-}
+// @Injectable({
+//   providedIn: 'root'
+// }),
+// export class AuthGuard implements CanActivate {
+//   // constructor(private authService: AuthService) { }
+// }
